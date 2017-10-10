83dbf7e55052e34464380470fcf0be7660759cb8
rloth <romain.loth@iscpif.fr>
2017-10-10 12:35:25 +0200

    Revert "do not activate multimatch yet"
    This reverts commit 4c8ef1eba166544918d09dd0b5ed734cd771a244.

--- static/tinawebJS/twpresets/settings_explorerjs.comex.js
+++ static/tinawebJS/twpresets/settings_explorerjs.comex.js
@@ -23,7 +23,7 @@ TW.conf = (function(TW){
   TWConf.sourceAPI={}
   TWConf.sourceAPI["nodetypes"] = {"node0": "Keywords", "node1": "Scholars" }
   TWConf.sourceAPI["forNormalQuery"] = "services/api/graph"
-  TWConf.sourceAPI["forFilteredQuery"] = "services/api/graph"
+  TWConf.sourceAPI["forFilteredQuery"] = "services/api/multimatch"
 
   // "services/api/graph" : traditional match with BipartiteExtractor
   // "services/api/multimatch" : new match inspired by BipartiteExtractor but all SQL and more generic
