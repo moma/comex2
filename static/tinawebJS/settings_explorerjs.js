fb55d3e1092f80126e18f5dea09f24fe8fb49385
rloth <romain.loth@iscpif.fr>
2017-10-27 18:48:16 +0200

    settings: multimatch on

--- static/tinawebJS/settings_explorerjs.js
+++ static/tinawebJS/settings_explorerjs.js
@@ -24,7 +24,7 @@ TW.conf = (function(TW){
   // ...or remote bridge to default source api ajax queries
   TWConf.sourceAPI={}
   TWConf.sourceAPI["nodetypes"] = {"node0": "Keywords", "node1": "Scholars" }
-  TWConf.sourceAPI["forNormalQuery"] = "services/api/graph"
+  TWConf.sourceAPI["forNormalQuery"] = "services/api/multimatch"
   TWConf.sourceAPI["forFilteredQuery"] = "services/api/multimatch"
 
   // "services/api/graph" : traditional match with BipartiteExtractor
