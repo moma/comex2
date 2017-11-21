68d52a5798d7b7c0c34cc5ca3dfb3470d65064c7
rloth <romain.loth@iscpif.fr>
2017-11-21 13:14:14 +0000

    clust louvain for default coloring

--- static/tinawebJS/settings_explorerjs.js
+++ static/tinawebJS/settings_explorerjs.js
@@ -64,7 +64,7 @@ TW.conf = (function(TW){
   TWConf.scanAttributes = true
 
   // use a facet for default color
-  TWConf.defaultColoring = "country"
+  TWConf.defaultColoring = "clust_louvain"
 
   // facetOptions: choose here the default visual result of your node attributes
   // ---------------------------------------------------------------------------
